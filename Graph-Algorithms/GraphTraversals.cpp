090b6449d0bcf62ac660d0f7e51a149ca0e405c8
Bence Racz <racz.bence@yahoo.com>
2024-03-24 14:12:25 +0200

    Added GraphTraversals.h for easier reuse of the algorithms

--- Graph-Algorithms/GraphTraversals.cpp
+++ Graph-Algorithms/GraphTraversals.cpp
@@ -1,13 +1,15 @@
 #include <stack>
 #include <queue>
 #include "Utils.h"
-
-using namespace std;
+#include "GraphTraversals.h"
 
 #define PRINT_PATH true
 
 // This function implements the Depth first search algorithm in an iterative manner
 void dfs_itr(const vector<vector<ui>>& adj_list, const ui& n, const ui& source_node) {
+	if (PRINT_PATH) {
+		cout << "Deapth First Search: ";
+	}
 	bool* visited = new bool[n] {0};
 	ui cur_node;
 	stack<ui> stack;
@@ -36,6 +38,9 @@ void dfs_itr(const vector<vector<ui>>& adj_list, const ui& n, const ui& source_n
 
 // This function implements the Breadth first search algorithm in an iterative manner
 void bfs_itr(const vector<vector<ui>>& adj_list, const ui& n, const ui& source_node) {
+	if (PRINT_PATH) {
+		cout << "Breadth First Search: ";
+	}
 	bool* visited = new bool[n] {0};
 	ui cur_node;
 	queue<ui> queue;
